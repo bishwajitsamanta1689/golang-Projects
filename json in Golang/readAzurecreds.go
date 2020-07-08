fa306abe40c35a03e07ce588d844479cbc51d6d2
bishwajitsamanta1689 <bishwajitsamanta1689@gmail.com>
2020-07-08 20:57:24 +0530

    Added Struct fields for Json

--- json in Golang/readAzurecreds.go
+++ json in Golang/readAzurecreds.go
@@ -10,11 +10,19 @@ type Azure struct{
 	Database struct {
 		Host string `json:"host"`
 		Port string `json:"port"`
-	}`json:"Database"`
+	}`json:"database"`
 	Credentials struct {
 		StorageAccountName string `json:"storage_account_name"`
 		SecondaryAccessKey string `json:"secondary_access_key"`
-	}`json:credentials`
+	}`json:"credentials"`
+	Secrets []struct {
+		Labels string `json:"labels"`
+		Azure struct {
+			StorageAccount string `json:"storage_account_name"`
+			PrimaryAccessKey string `json:"primary_access_key"`
+			SecondaryAccessKey string `json:"secondary_access_key"`
+		}`json:"Azure"`
+	}`json:"secrets"`
 }
 
 func LoadConfiguration(filename string) (Azure,error) {
@@ -31,5 +39,14 @@ func LoadConfiguration(filename string) (Azure,error) {
 
 func main(){
 	config, _:= LoadConfiguration("filetointerpolate.json")
+	fmt.Println("Database Host=", config.Database.Host)
 	fmt.Println("Secondary Access Key=", config.Credentials.SecondaryAccessKey)
+	array := config.Secrets
+	for _, keysStruct:= range array{
+		fmt.Println("label=", keysStruct.Labels)
+	}
+	for _, keys:= range array{
+		fmt.Println("Primary Access Key=", keys.Azure.PrimaryAccessKey)
+	}
+
 }
