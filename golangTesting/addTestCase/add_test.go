7fe23ff9d79bde55d1ab8cddcc36681d94b95576
bishwajitsamanta1689 <bishwajitsamanta1689@gmail.com>
2020-07-15 10:17:57 +0530

    Mocking Test Case for HTTP Request

--- golangTesting/addTestCase/add_test.go
+++ golangTesting/addTestCase/add_test.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -34,4 +38,22 @@ func TestStringData(t *testing.T){
 	if string(data) != "key = value" {
 		t.Fatal("Expected Text not Matched")
 	}
+}
+
+func TestHttpRequest(t *testing.T){
+	handler := func(w http.ResponseWriter, r *http.Request){
+		io.WriteString(w,"{\"status\":\"working\"}")
+	}
+
+	req:= httptest.NewRequest("GET", "https://google.com", nil)
+	w:= httptest.NewRecorder()
+	handler(w,req)
+
+	resp:= w.Result()
+	body, _:= ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
+	if 200 != resp.StatusCode{
+		t.Fatal("Status Code Not Ok")
+	}
+
 }
