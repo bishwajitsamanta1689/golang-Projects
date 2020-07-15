59797f4d55b22e0bd7d5c5591c9ae23e9c5512e9
bishwajitsamanta1689 <bishwajitsamanta1689@gmail.com>
2020-07-15 08:46:07 +0530

    Adding Test Case for Addition

--- golangTesting/addTestCase/add_test.go
+++ golangTesting/addTestCase/add_test.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"testing"
+)
+
+// Test Driven Test Cases in Golang
+
+type AddResult struct{
+	x int
+	y int
+	expected int
+}
+
+var addResults = []AddResult{
+	{1, 1,2},
+	{2,3,5},
+}
+
+func TestAdd(t *testing.T){
+	for _, test:= range addResults{
+		result := Add(test.x, test.y)
+		if result != test.expected {
+			t.Fatal("Expected result not given")
+		}
+	}
+}
